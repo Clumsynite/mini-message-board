6662f3ff674166bae3ecc8fb8a09bc50995134f9
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-09-28 09:56:13 +0530

    Removed unnecesarry files

--- routes/new.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/new', function(req, res, next) {
-  res.render('new_form', { title: 'New Message' });
-});
-
-module.exports = router;
