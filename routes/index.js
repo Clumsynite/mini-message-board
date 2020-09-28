555274b18783c8bc0a39b140935f0a6bb04082e1
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-09-28 09:55:43 +0530

    Added next message router

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,42 @@
 const express = require('express');
+
 const router = express.Router();
-const moment = require('moment')
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: moment(new Date()).format('LLLL')
+const moment = require('moment');
+
+const messages = [{
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
   },
   {
-    text: "Hello World!",
-    user: "Charles",
-    added: moment(new Date()).format('LLLL')
-  }
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
 ];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mini Messageboard', messages: messages });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Mini Messageboard',
+    messages
+  });
 });
 
+router.get('/new', (req, res, next) => {
+  res.render('new_message', {
+    title: 'New Message'
+  })
+})
+
+router.post('/new', (req, res, next) => {
+  const message = {
+    user: req.body.author,
+    text: req.body.text,
+    added: new Date()
+  }
+  messages.push(message)
+  // res.render('index', {title: 'Mini MessageBoard', messages: messages})
+  res.redirect('/')
+})
 module.exports = router;
