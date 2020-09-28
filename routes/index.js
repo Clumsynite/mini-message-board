497fcededfdd152d52da9bb5a7bae172fc7a26b4
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-09-28 07:32:52 +0530

    Formatted Date

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
-
+const moment = require('moment')
 const messages = [
   {
     text: "Hi there!",
     user: "Amando",
-    added: new Date()
+    added: moment(new Date()).format('LLLL')
   },
   {
     text: "Hello World!",
     user: "Charles",
-    added: new Date()
+    added: moment(new Date()).format('LLLL')
   }
 ];
 
